9f2f9cb414ab880231ac3086080cadd4df9f0ff5
Edwin Njeru <kamuruaedwin@gmail.com>
2023-10-20 20:24:45 +0300

    bootstrap added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,34 +7,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.new(phone_number: params[:phone_number])
+    @user = User.find_by(phone_number: params[:phone_number])
 
-    if @user.authenticate(params[:password]) && @user.save
+    if @user&.authenticate(params[:password])
       @user.update(logged_in: true)
-      login @user
-      redirect_to root_path, notice: "You have logged in successfully"
+      login(@user)
+      redirect_to root_path, notice: "You have logged in successfully."
     else
-      # You should populate the errors on @user if authentication fails
-      @user.errors.add(:base, "Invalid phone number or password.")
+      flash.now[:alert] = "Invalid phone number or password."
       render :new, status: :unprocessable_entity
     end
-        # if user = User.authenticate_by(phone_number: params[:phone_number], password: params[:password])
-    #   user.update(logged_in: true)
-    #   login user
-    #   redirect_to root_path, notice: "You have logged in successfully"
-    # else
-    #   flash[:alert] = "Invalid phone number or password."
-    #   render :new, status: :unprocessable_entity
-    # end
   end
 
   def destroy
     if user_signed_in?
       current_user.update(logged_in: false)
       reset_session
-      redirect_to root_path, notice: "You have been logged out successfully"
+      redirect_to root_path, notice: "You have been logged out successfully."
     else
-      redirect_to root_path, notice: "Failed to Log Out."
+      redirect_to root_path, notice: "Failed to log out."
     end
   end
 
@@ -45,4 +36,3 @@ class SessionsController < ApplicationController
     @online_users_count = User.where(logged_in: true).count
   end
 end
-
