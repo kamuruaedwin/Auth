c6bda8a7f670950135522e043bf01dc7b92bcd6d
Edwin Njeru <kamuruaedwin@gmail.com>
2023-11-06 11:40:03 +0300

    chat improved

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
 		
 		@messages = Message.order(created_at: :desc)
 		@messages = Message.all # Or fetch messages from your database
-
+		@message = Message.new #initializing an empty message object for the form
 	    @bet = Bet.new
 	    @bet.betid = SecureRandom.hex(6) # Generate a 6-character hexadecimal betid
   	end
